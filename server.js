36b3f43ee4ef53d2cf1d3e970939bdc6514792c3
Verve-Kalpa <sw5@verveautomation.com>
2025-06-23 10:18:44 +0530

    DB connectivity

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const sequelize = require("./config/db");
 const app = express();
 
 const PORT = process.env.PORT || 5000;
@@ -10,7 +11,11 @@ app.use(express.json());
 app.use(cors());
 
 // Routes
-app.use("", require("./routes/"));
-
+// app.use("", require("./routes/"));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+sequelize
+  .sync({ alter: true }) // Updates tables without dropping data
+  .then(() => console.log("✅ Database & tables synced successfully!"))
+  .catch((err) => console.error("❌ Error syncing database:", err));
