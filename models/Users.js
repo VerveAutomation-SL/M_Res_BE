72e691edc961ea502183d26051ca338e42d48ed0
Verve-Kalpa <sw5@verveautomation.com>
2025-06-23 11:37:07 +0530

    Create Users.js

---
+++ models/Users.js
@@ -0,0 +1,45 @@
+const sequelize = require("../config/db");
+const { DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
+
+const User = sequelize.define("User", {
+    UserId : {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    }, {
+    timestamps: true,
+    tableName: 'users',
+
+    hooks: {
+        beforeCreate: async (user) => {
+            if (user.password) {
+                const salt = await bcrypt.genSalt(10);
+                user.password = await bcrypt.hash(user.password, salt);
+            }
+        },
+        beforeUpdate: async (user) => {
+            if (user.changed('password')) {
+                const salt = await bcrypt.genSalt(10);
+                user.password = await bcrypt.hash(user.password, salt);
+            }
+        }
+    },
+})
+
+module.exports = User;
