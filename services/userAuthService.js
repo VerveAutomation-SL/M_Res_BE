fffcfc179162a9ea6592924be03ba472c47de42c
Verve-Kalpa <sw5@verveautomation.com>
2025-08-06 09:37:22 +0530

    Remove unused JWT import and simplify user data object in login service

--- services/userAuthService.js
+++ services/userAuthService.js
@@ -1,5 +1,4 @@
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
 const User = require("../models/Users");
 const { generateTokens } = require("./tokenService");
 const AppError = require("../utils/AppError");
@@ -20,7 +19,7 @@ const loginService = async ({ userName, password }) => {
             throw new AppError(401, "Invalid password");
         }
 
-        const userData = {UserId: user.UserId, role: user.role, username: user.username, email: user.email, permissionId: user.PermissionId};
+        const userData = {UserId: user.UserId, role: user.role, username: user.username, email: user.email};
 
         // Generate JWT tokens
         const accessToken = generateTokens(userData); 
