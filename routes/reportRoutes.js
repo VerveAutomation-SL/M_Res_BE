7bed05c3489e2587df74b5b643f6c989965d4f6f
Verve-Kalpa <sw5@verveautomation.com>
2025-08-14 15:58:27 +0530

    feat: add PDF report generation using pdfmake
    - Updated package.json and package-lock.json to include pdfmake dependency.
    - Modified reportRoutes.js to use the new generatePdfReport function.
    - Implemented generatePdfReportservice in reportService.js to create a detailed PDF report of guest check-ins with dynamic data and styling.

--- routes/reportRoutes.js
+++ routes/reportRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { generateExcelReportController, generatePDFReportController, getPreviewDataController } = require('../controllers/reportController');
+const { generateExcelReportController, generatePDFReportController, getPreviewDataController, generatePdfReport } = require('../controllers/reportController');
 
 router.post("/excel", generateExcelReportController);
-router.post("/pdf", generatePDFReportController);
+router.post("/pdf", generatePdfReport);
 router.post("/preview", getPreviewDataController);
 
 module.exports = router;
